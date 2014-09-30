d634df4a3c126ad5a3583dcea72b1d956b6669c6
Sævarður Einarsson <saevardur@gmail.com>
2014-09-30 22:03:10 +0000

    Initial commit

---
+++ Instruments/instrument.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Instrument.py: Basic instrument class."""
+
+__author__      = "Sævarður Einarsson"
+__copyright__   = "Copyright 2014, Arion Bank"
+
+class Instrument:
+  "Instrument"
+
+  flokkurid = 0
+  audkenni = null
+
+  def __init__(self):
+    "Init null instument"
+
+  def __init__(self,flokkurId):
+    "Init instrument with flokkurID"
+
+  def __init__(self,audkenni):
+    "Init instrument with audkenni"
---
+++ Portfolios/portfolio.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Portfolio.py: Arion bank asset holding portfolio."""
+
+__author__      = "Sævarður Einarsson"
+__copyright__   = "Copyright 2014, Arion Bank"
+
+class Portfolio:
+	"Portfolio"
+
+	portfolioID = null
+	assets = null
+
+	def __init__(self):
+		"Init null portfolio"
+
+	def __init__(self,pID):
+		"Init empty portfolio"
+
+		self.portfolioID = pID
+
+	def calculateValue(date):
+		"Calculate portfolio value on a date. Default today"
+
+	def addAsset(asset):
+		"Add a single asset to the portfolio"
+
+	def addAssets(assets):
+		"Add a list of assets to the portfolio"
