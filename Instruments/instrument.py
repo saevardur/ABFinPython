d3d262ba307174e2db4e5374238097273fc81c9c
Sævarður Einarsson <saevardur@gmail.com>
2014-10-02 22:41:19 +0000

    Föll og gagnatenging
    Byrjað að setja up föll og gagnatengingar

--- Instruments/instrument.py
+++ Instruments/instrument.py
@@ -1,21 +1,57 @@
 #!/usr/bin/env python
 
+import dataaccess as da
+import pandas as pd
+
 """Instrument.py: Basic instrument class."""
 
-__author__      = "Sævarður Einarsson"
+__author__      = "Saevardur Einarsson"
 __copyright__   = "Copyright 2014, Arion Bank"
 
 class Instrument:
-  "Instrument"
+    """Instrument
 
-  flokkurid = 0
-  audkenni = null
+    Attributes:
+        flokkurID:
+        audkenni:
+    """
+    flokkurID = null
+    audkenni = null
+    prices = null
+    yields = null # bond class?
+    cleanPrices = null # bond class?
 
-  def __init__(self):
-    "Init null instument"
 
-  def __init__(self,flokkurId):
-    "Init instrument with flokkurID"
+    def __init__(self):
+        'Init null instument'
 
-  def __init__(self,audkenni):
-    "Init instrument with audkenni"
+    def __init__(self,flokkurID):
+        'Init instrument with flokkurID'
+        self.flokkurID = flokkurID
+        self.audkenni = da.getAudkenni(flokkurID)
+
+    def __init__(self,audkenni):
+        'Init instrument with audkenni'
+        self.audkenni = audkenni
+        self.flokkurID = da.getFlokkurID(audkenni)
+
+    def getClosingPrice(date):
+        'Get closing price for date from Vog. Default last business day.'
+
+    def getClosingYield(date):
+        'Get closing yield for date from Vog. Default last business day. Only works for bonds, should maybe be another class.'
+
+    def calculateReturns(date):
+        'Calculate returns. Should probably take in date range, where the default for starting date is the first date and ending date is last business day.'
+
+    def calculateLogReturns(date):
+        'Calculate log returns. Should probably take in date range, where the default for starting date is the first date and ending date is last business day.'
+
+    def plotPrice():
+        'Plot price'
+
+    def plotReturns():
+        'Plot returns'
+
+    def plotLogReturns():
+        'Plot returns'
--- Portfolios/portfolio.py
+++ Portfolios/portfolio.py
@@ -1,29 +1,71 @@
 #!/usr/bin/env python
 
+import instrument as inst
+
 """Portfolio.py: Arion bank asset holding portfolio."""
 
-__author__      = "Sævarður Einarsson"
+__author__      = "Saevardur Einarsson"
 __copyright__   = "Copyright 2014, Arion Bank"
 
 class Portfolio:
-	"Portfolio"
+    """Portfolio class
 
-	portfolioID = null
-	assets = null
+    Attributes:
+        portfolioID: The ID of the portfolio as defined in VBR (safnID).
+        assets: A list of Holding instances which hold an asset and the nominal value contained in the portfolio.
+    """
+    portfolioID = null
+    weightsPortfolio = false
+    assets = null
 
 	def __init__(self):
-		"Init null portfolio"
+		'Init null portfolio'
 
 	def __init__(self,pID):
-		"Init empty portfolio"
+		'Init empty portfolio'
 
 		self.portfolioID = pID
 
-	def calculateValue(date):
-		"Calculate portfolio value on a date. Default today"
-
-	def addAsset(asset):
-		"Add a single asset to the portfolio"
+	def addAsset(flokkurID,nominal):
+		'Add a single asset to the portfolio'
+        if (nominal < 1):
+            # If nominal is less we assume it refers to the percentage weight.
+            self.weightsPortfolio = true
+            self.assets.append(Holding(flokkurID,nominal))
 
 	def addAssets(assets):
-		"Add a list of assets to the portfolio"
+		'Add a list of assets to the portfolio'
+
+    def calculateMarketValue(date):
+		'Calculate portfolio market value on a date. Default date is today'
+
+    def calculateReturns(date):
+        'Calculate portfolio returns on a date. Default date is today'
+
+class Holding:
+    """Portfolio Holding class
+
+    Attributes
+        asset: A instance of the instrument class.
+        nominal: The nominal amount of the asset (can also be a weight if it is less than 1).
+        buyYield: The yield at which the asset was bought.
+    """
+    asset = null
+    nominal = null
+    buyYield = null
+
+    def __init__(self):
+		'Init null holding'
+
+	def __init__(self,flokkurID,nominal=0):
+		'Init holding with instrument'
+        self.asset = inst.Instrument(flokkurID)
+        self.nominal = nominal
+
+    def calculateMarketValue(date):
+        'Calculate market value of holding on date. (Only works if working with nominal amount)'
+        return nominal * self.asset.getClosingPrice(date)
+
+    def calculateReturns(date):
+        'Calculate holding returns on a date. Default date is today'
+        # get straight from instrument
